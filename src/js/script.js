44ec9edcddc2a5f0919b9741146ab79b29cb7e59
Eric Sweetland <eric.sweetland@dominos.com>
2020-10-09 11:56:45 -0400

    getting the map to show up on the page

---
+++ src/js/script.js
@@ -0,0 +1,9 @@
+let map;
+
+function initMap() {
+  console.log("init");
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
