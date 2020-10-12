75b50433c7fc224df53b36887ebbd43b97e8ce24
Eric Sweetland <eric.sweetland@dominos.com>
2020-10-12 14:34:01 -0400

    Add Smoothing Algorithm * added douglasPeucker

--- src/js/script.js
+++ src/js/script.js
@@ -28,15 +28,15 @@ function initMap() {
 
   google.maps.event.addListener(map, "mousemove", function (e) {
     if (isDrawing == true) {
-      let pageX = e.pixel.x;
-      let pageY = e.pixel.y;
-      let point = new google.maps.Point(parseInt(pageX), parseInt(pageY));
+      const pageX = e.pixel.x;
+      const pageY = e.pixel.y;
+      const point = new google.maps.Point(parseInt(pageX), parseInt(pageY));
       let latLng = overlay.getProjection().fromContainerPixelToLatLng(point);
       polyLine.getPath().push(latLng);
       latLng = String(latLng);
       latLng = latLng.replace("(", "");
       latLng = latLng.replace(")", "");
-      let array_lng = latLng.split(",");
+      const array_lng = latLng.split(",");
       pointsArray.push(new google.maps.LatLng(array_lng[0], array_lng[1]));
     }
   });
@@ -54,9 +54,115 @@ function initMap() {
         editable: true,
         geodesic: false,
       });
+
+      //simplify polygon
+      var douglasPeuckerThreshold = 100; // in meters
+      polygon.douglasPeucker(
+        douglasPeuckerThreshold / (2.0 * Math.PI * earthRadius)
+      );
+
       polygon.setMap(map);
       polyLine.setMap(null);
       pointsArray = [];
     })
   );
+
+  const earthRadius = 6378137.0;
+
+  google.maps.Polygon.prototype.douglasPeucker = function (tolerance) {
+    let res = null;
+
+    //adjust tolerance depending on the zoom level
+    tolerance = tolerance * Math.pow(2, 20 - map.getZoom());
+
+    if (this.getPath() && this.getPath().getLength()) {
+      const points = this.getPath().getArray();
+
+      const Line = function (p1, p2) {
+        this.p1 = p1;
+        this.p2 = p2;
+
+        this.distanceToPoint = function (point) {
+          // slope
+          const m =
+              (this.p2.lat() - this.p1.lat()) / (this.p2.lng() - this.p1.lng()),
+            // y offset
+            b = this.p1.lat() - m * this.p1.lng(),
+            d = [];
+          // distance to the linear equation
+          d.push(
+            Math.abs(point.lat() - m * point.lng() - b) /
+              Math.sqrt(Math.pow(m, 2) + 1)
+          );
+          // distance to p1
+          d.push(
+            Math.sqrt(
+              Math.pow(point.lng() - this.p1.lng(), 2) +
+                Math.pow(point.lat() - this.p1.lat(), 2)
+            )
+          );
+          // distance to p2
+          d.push(
+            Math.sqrt(
+              Math.pow(point.lng() - this.p2.lng(), 2) +
+                Math.pow(point.lat() - this.p2.lat(), 2)
+            )
+          );
+          // return the smallest distance
+          return d.sort(function (a, b) {
+            return a - b; //causes an array to be sorted numerically and ascending
+          })[0];
+        };
+      };
+
+      const douglasPeucker = function (points, tolerance) {
+        if (points.length <= 2) {
+          return [points[0]];
+        }
+        let returnPoints = [],
+          // make line from start to end
+          line = new Line(points[0], points[points.length - 1]),
+          // find the largest distance from intermediate poitns to this line
+          maxDistance = 0,
+          maxDistanceIndex = 0,
+          p;
+        console.log(points);
+        for (var i = 1; i <= points.length - 2; i++) {
+          const distance = line.distanceToPoint(points[i]);
+          console.log(distance, maxDistance);
+          if (distance > maxDistance) {
+            maxDistance = distance;
+            maxDistanceIndex = i;
+          }
+        }
+        // check if the max distance is greater than our tollerance allows
+        if (maxDistance >= tolerance) {
+          p = points[maxDistanceIndex];
+          line.distanceToPoint(p, true);
+          // include this point in the output
+          returnPoints = returnPoints.concat(
+            douglasPeucker(points.slice(0, maxDistanceIndex + 1), tolerance)
+          );
+          // returnPoints.push( points[maxDistanceIndex] );
+          returnPoints = returnPoints.concat(
+            douglasPeucker(
+              points.slice(maxDistanceIndex, points.length),
+              tolerance
+            )
+          );
+        } else {
+          // ditching this point
+          p = points[maxDistanceIndex];
+          line.distanceToPoint(p, true);
+          returnPoints = [points[0]];
+        }
+        return returnPoints;
+      };
+      res = douglasPeucker(points, tolerance);
+      // always have to push the very last point on so it doesn't get left off
+      res.push(points[points.length - 1]);
+      this.setPath(res);
+    }
+    return this;
+  };
 }
